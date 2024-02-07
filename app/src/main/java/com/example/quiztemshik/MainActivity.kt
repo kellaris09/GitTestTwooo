62be8c0d9805e047583af613503032b422870a60
Alexander Murashev <grinsky99@gmail.com>
2024-02-07 11:17:33 +0300

    Create Main Page * Создано то то и то

--- app/src/main/java/com/example/quiztemshik/MainActivity.kt
+++ app/src/main/java/com/example/quiztemshik/MainActivity.kt
@@ -7,7 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("Hello Main Page")
+
 
 
     }
+
 }
